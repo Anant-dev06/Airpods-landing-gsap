ad3e24f0bc963127463374aaebe5a2641521b072
Anant Shrotriya <shrotriyaanant2006@gmail.com>
2026-06-01 15:40:59 +0530

    added scrollTrigger

--- script.js
+++ script.js
@@ -45,35 +45,40 @@ gsap.from("#right-hro img", {
 
 gsap.from("#pg2-img", {
     x: -100,
-    delay: 12,
+    delay: 2,
     duration: 1.2,
-    opacity: 0
+    opacity: 0,
+    scrollTrigger: "#pg2-img"
 })
 
 gsap.from("#pg2-head", {
     x: 200,
-    delay: 12,
+    delay: 2,
     duration: 1.8,
-    opacity: 0
+    opacity: 0,
+    scrollTrigger: "#pg2-head"
 })
 
 gsap.from("#pg2-para", {
     x: 200,
-    delay: 12.5,
+    delay: 2,
     duration: 1.8,
-    opacity: 0
+    opacity: 0,
+    scrollTrigger: "#pg2-para"
 })
 
 gsap.from("#pg2-btn", {
     x: -200,
-    delay: 12.7,
     duration: 0.8,
-    opacity: 0
+    delay: 2,
+    opacity: 0,
+    scrollTrigger: "#pg2-btn"
 })
 
 gsap.from("#pg3-img", {
     x: 200,
-    delay: 16.5,
+    delay: 4,
     duration: 1,
-    opacity: 0
+    opacity: 0,
+    scrollTrigger: "#pg3-img"
 })
